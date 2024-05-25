068c2816b8246e9503b363c197d33723d9a55f64
Dawid Manikowski <dd.manikowski@gmail.com>
2024-05-25 18:35:44 +0200

    Player moving on map

--- player.gd
+++ player.gd
@@ -1,4 +1,4 @@
-extends Area2D
+extends CharacterBody2D
 
 
 const SPEED = 300.0
@@ -6,7 +6,7 @@ const SPEED = 300.0
 func _ready():
 	pass
 
-func _process(delta):
+func _physics_process(delta):
 	var velocity = Vector2.ZERO
 	if Input.is_action_pressed("walk_right"):
 		$AnimatedSprite2D.animation = "walk_right"
@@ -21,7 +21,7 @@ func _process(delta):
 		$AnimatedSprite2D.animation = "walk_down"
 		velocity.y += 1 * SPEED
 	$AnimatedSprite2D.play()
-	position += velocity * delta
+	move_and_collide(velocity*delta)
 	#if velocity.length() > 0:
 		#velocity = velocity.normalized() * SPEED
 		#$AnimatedSprite2D.play()
