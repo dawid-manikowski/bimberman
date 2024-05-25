ec11600151ddbd8a7cd7e332e86f512c14b7515a
Dawid Manikowski <dd.manikowski@gmail.com>
2024-05-25 18:05:33 +0200

    Move player faster

--- player.gd
+++ player.gd
@@ -9,19 +9,17 @@ func _ready():
 func _process(delta):
 	var velocity = Vector2.ZERO
 	if Input.is_action_pressed("walk_right"):
-		print("right")
 		$AnimatedSprite2D.animation = "walk_right"
-		velocity.x += 1
-	if Input.is_action_pressed("walk_left"):
-		print("left")
+		velocity.x += 1 * SPEED
+	elif Input.is_action_pressed("walk_left"):
 		$AnimatedSprite2D.animation = "walk_left"
-		velocity.x -= 1
-	if Input.is_action_pressed("walk_up"):
+		velocity.x -= 1 * SPEED
+	elif Input.is_action_pressed("walk_up"):
 		$AnimatedSprite2D.animation = "walk_up"
-		velocity.y -= 1
-	if Input.is_action_pressed("walk_down"):
+		velocity.y -= 1 * SPEED
+	elif Input.is_action_pressed("walk_down"):
 		$AnimatedSprite2D.animation = "walk_down"
-		velocity.y += 1
+		velocity.y += 1 * SPEED
 	$AnimatedSprite2D.play()
 	position += velocity * delta
 	#if velocity.length() > 0:
